e15e1beb5d5a2acddfa5c9098fc7106179fbc76a
Helmut Grohne <h.grohne@cygnusnetworks.de>
2013-07-23 11:59:23 +0200

    license as GPL-2

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
 		author_email="h.grohne@cygnusnetworks.de",
 		maintainer="Cygnus Networks GmbH",
 		maintainer_email="info@cygnusnetworks.de",
+		license="GPL-2",
 		packages=["nssct", "nssct.backend", "nssct.plugins"],
 		test_suite="unittest2.collector",
 		entry_points=dict(console_scripts=["nssct=nssct.main:main"]),
