f02f3097a4d2bc9eda6b7700625ec9d819a1b81a
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2013-08-07 12:28:05 +0200

    Changed version for initial release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 		name="nssct",
-		version="0.0",
+		version="0.10",
 		author="Helmut Grohne",
 		author_email="h.grohne@cygnusnetworks.de",
 		maintainer="Cygnus Networks GmbH",
